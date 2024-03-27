3b0c2dc7d517bc610edc0b17c2d6b1bbe1896f9a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-03-27 15:36:32 +0200

    eka

---
+++ teht2/t2.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    print_r($_POST);
+}
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+
+    <label for="remember-me">Username</label>
+    <input type="checkbox" name="remember-me" id="remember-me">
+
+    <button type="submit">Submit</button>
+</form>
