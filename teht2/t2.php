6ce6dba741248cd7ac4f1667360fd3957d7fbf50
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-03-27 15:49:50 +0200

    eka

--- teht2/t2.php
+++ teht2/t2.php
@@ -1,15 +1,28 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    print_r($_POST);
+    if (isset($_POST['remember-me'])) {
+        setcookie('username', $_POST['username']);
+    } else {
+        setcookie('username', '', time() - 3600);
+    }
 }
 ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <label for="username">Username</label>
-    <input type="text" name="username" id="username">
+    <input <?php
+    if (isset($_COOKIE['username'])) {
+        echo 'value="' . $_COOKIE['username'] . '"';
+    }
+    ?> type="text" name="username" id="username">
 
     <label for="remember-me">Remember me</label>
-    <input type="checkbox" name="remember-me" id="remember-me">
+    <input
+        <?php
+        if (isset($_COOKIE['username'])) {
+            echo 'checked';
+        }
+        ?> type="checkbox" name="remember-me" id="remember-me">
 
     <button type="submit">Submit</button>
 </form>
