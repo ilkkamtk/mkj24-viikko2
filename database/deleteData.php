7a203491a9a0992461d92105f03057ed8b0c212f
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-04-03 15:05:27 +0300

    delete-fix

--- database/deleteData.php
+++ database/deleteData.php
@@ -13,26 +13,6 @@ if(isset($_GET['id'])) {
     $data = [
         'media_id' => $_GET['id'],
     ];
-    // delete file from server
-    $sql = 'SELECT filename FROM MediaItems WHERE media_id = :media_id';
-    try {
-        $STH = $DBH->prepare($sql);
-        $STH->execute($data);
-        $STH->setFetchMode(PDO::FETCH_ASSOC);
-        $row = $STH->fetch();
-        $filename = $row['filename'];
-        $destination = __DIR__ . '/uploads/' . $filename;
-        if (!unlink($destination)) {
-            echo "Could not delete file from the server.";
-            $DBH->rollBack();
-            exit;
-        }
-    } catch (PDOException $e) {
-        echo "Could not get filename from the database.";
-        file_put_contents('PDOErrors.txt', 'deleteData.php - ' . $e->getMessage(), FILE_APPEND);
-        $DBH->rollBack();
-        exit;
-    }
 
     // delete likes
     $sql = 'DELETE FROM Likes WHERE media_id = :media_id';
@@ -85,6 +65,28 @@ if(isset($_GET['id'])) {
     }
 
     $data['user_id'] = $_SESSION['user']['user_id'];
+
+    // delete file from server
+    $sql = 'SELECT filename FROM MediaItems WHERE media_id = :media_id AND user_id = :user_id';
+    try {
+        $STH = $DBH->prepare($sql);
+        $STH->execute($data);
+        $STH->setFetchMode(PDO::FETCH_ASSOC);
+        $row = $STH->fetch();
+        $filename = $row['filename'];
+        $destination = __DIR__ . '/uploads/' . $filename;
+        if (!unlink($destination)) {
+            echo "Could not delete file from the server.";
+            $DBH->rollBack();
+            exit;
+        }
+    } catch (PDOException $e) {
+        echo "Could not get filename from the database.";
+        file_put_contents('PDOErrors.txt', 'deleteData.php - ' . $e->getMessage(), FILE_APPEND);
+        $DBH->rollBack();
+        exit;
+    }
+
     // delete MediaItems
     $sql = 'DELETE FROM MediaItems WHERE media_id = :media_id AND user_id = :user_id';
 
